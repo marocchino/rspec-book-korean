ebf7849e33d3ccb8529db3a5b6c8778c5ef86310
Shim Taewon <marocchino@gmail.com>
2010-03-27 10:16:12 +0900

    테스트고드 작성

---
+++ features/step_definitions/mastermind_ko.rb
@@ -0,0 +1,17 @@
+
+Given /^나는 아직 시작하지 않았습니다$/ do
+	  pending # express the regexp above with the code you wish you had
+end
+
+When /^새 게임을 시작합니다$/ do
+	  pending # express the regexp above with the code you wish you had
+end
+
+Then /^화면에 "([^\"]*)"를 표시해야 합니다$/ do |arg1|
+	  pending # express the regexp above with the code you wish you had
+end
+
+Then /^화면에 "([^\"]*)"을 표시해야 합니다$/ do |arg1|
+	  pending # express the regexp above with the code you wish you had
+end
+
