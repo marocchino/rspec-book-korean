03e2b5a27a996058d08c87f3109a46075503e299
Tw Shim <marocchino@gmail.com>
2013-12-02 12:06:23 +0900

    update cucumber codes

--- mastermind/features/step_definitions/mastermind_ko.rb
+++ mastermind/features/step_definitions/mastermind_ko.rb
@@ -1,13 +1,13 @@
-
+require 'mastermind'
 Given /^나는 아직 시작하지 않았습니다$/ do
 end
 
 When /^새 게임을 시작합니다$/ do
-	@messenger = StringIO.new
-	game = Mastermind::Game.new(@messenger)	
-	@message = game.start
+  @messenger = StringIO.new
+  game = Mastermind::Game.new(@messenger)
+  @message = game.start
 end
 
 Then /^화면에 "([^\"]*)"(?:을|를) 표시해야 합니다$/ do |message|
-	@messenger.string.split("\n").should include(message)
+  @messenger.string.split("\n").should include(message)
 end
