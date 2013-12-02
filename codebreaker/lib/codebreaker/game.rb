e2d8558000f9ab6b2ba007fea5ab6138ba844c22
Tw Shim <marocchino@gmail.com>
2013-12-02 16:38:13 +0900

    ch. 7

--- codebreaker/lib/codebreaker/game.rb
+++ codebreaker/lib/codebreaker/game.rb
@@ -7,10 +7,20 @@ module Codebreaker
     def start(secret)
       @output.puts "코드 브레이커!"
       @output.puts "입력:"
+      @secret = secret.split("").map(&:to_i)
     end
 
     def guess(guess)
+      @guess = guess.split("").map(&:to_i)
+      @output.puts mark
+    end
 
+    def mark
+      common_number = @secret & @guess
+      plus_size = common_number.select { |i| @secret.index(i) == @guess.index(i) }.size
+      minus_size = common_number.size - plus_size
+
+      "+" * plus_size + "-" * minus_size
     end
   end
 end
--- codebreaker/spec/codebreaker/game_spec.rb
+++ codebreaker/spec/codebreaker/game_spec.rb
@@ -1,9 +1,9 @@
 require File.join(File.dirname(__FILE__), "/../spec_helper")
 module Codebreaker
   describe Game do
-    context "starting up" do
-      let(:output) { double("output").as_null_object }
-      let(:game) { Game.new(output) }
+    let(:output) { double("output").as_null_object }
+    let(:game) { Game.new(output) }
+    describe "#start" do
       it "send a welcome message" do
         output.should_receive(:puts).with("코드 브레이커!")
         game.start('1234')
@@ -12,6 +12,49 @@ module Codebreaker
         output.should_receive(:puts).with("입력:")
         game.start('1234')
       end
+    end
+    describe "#guess" do
+      context "with no matches" do
+        it "sends mark with ''" do
+          game.start('1234')
+          output.should_receive(:puts).with('')
+          game.guess('5555')
+        end
+      end
+
+      context "with 1 number matches" do
+        it "sends mark with '-'" do
+          game.start('1234')
+          output.should_receive(:puts).with('-')
+          game.guess('2555')
+        end
+
+        it "sends mark with '+'" do
+          game.start('1234')
+          output.should_receive(:puts).with('+')
+          game.guess('1555')
+        end
+      end
+
+      context "with 2 number matches" do
+        it "sends mark with '--'" do
+          game.start('1234')
+          output.should_receive(:puts).with('--')
+          game.guess('2355')
+        end
+
+        it "sends mark with '+-'" do
+          game.start('1234')
+          output.should_receive(:puts).with('+-')
+          game.guess('3255')
+        end
+        it "sends mark with '++'" do
+          game.start('1234')
+          output.should_receive(:puts).with('++')
+          game.guess('1255')
+        end
+      end
+
     end
   end
 end
